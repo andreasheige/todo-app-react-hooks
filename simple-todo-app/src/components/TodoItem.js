b8bc6c122f1de25f121fd066c3f9a1d5d7f34446
Andreas Heige <a.heige.83@gmail.com>
2020-06-16 09:58:42 +0200

    refactoring TodoItem

--- simple-todo-app/src/components/TodoItem.js
+++ simple-todo-app/src/components/TodoItem.js
@@ -10,17 +10,19 @@ class TodoItem extends React.Component {
             opacity: 0.4,
             textDecoration: "line-through",
         }
+
+        const { completed, id, title } = this.props.todo
         
     return <li className="todo-item">
             <input  type="checkbox" 
-                    checked={this.props.todo.completed}
-                    onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                    checked={completed}
+                    onChange={() => this.props.handleChangeProps(id)}
             />
-            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+            <button onClick={() => this.props.deleteTodoProps(id)}>
                 Delete
             </button>
             <span style={this.props.todo.completed ? completedStyle : null}>
-                {this.props.todo.title}
+                {title}
             </span>
             </li>
     };
