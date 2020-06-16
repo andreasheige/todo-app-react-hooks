df7d17b8815d265de9bad6d73d2e835a1c7f0d49
Andreas Heige <a.heige.83@gmail.com>
2020-06-16 10:47:06 +0200

    Updates

---
+++ precache-manifest.24a9d5990c2151a78a38066f8c1da6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadf5d41491c9c576485a7ca1e71e7ae",
+    "url": "/todo-app-react-hooks/index.html"
+  },
+  {
+    "revision": "275882606fb86e56e8e6",
+    "url": "/todo-app-react-hooks/static/css/main.0e4e4cbf.chunk.css"
+  },
+  {
+    "revision": "24dd0e1ef7de038cf329",
+    "url": "/todo-app-react-hooks/static/js/2.44623fd5.chunk.js"
+  },
+  {
+    "revision": "275882606fb86e56e8e6",
+    "url": "/todo-app-react-hooks/static/js/main.1fa8ae0b.chunk.js"
+  },
+  {
+    "revision": "a5c12ffe26b50e8eabba",
+    "url": "/todo-app-react-hooks/static/js/runtime-main.ae7203c6.js"
+  }
+]);
